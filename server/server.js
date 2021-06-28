0eb3bb2a9bf6636789575d03cc66bd96a66e64ca
Christopher Campbell <campbefs@gmail.com>
2021-06-28 01:50:46 -0700

    concurrently set up

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 // import our typeDefs & resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -24,6 +25,15 @@ server.applyMiddleware({ app });
 // Create a new Apollo server and pass in our schema data 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
